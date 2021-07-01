04762361d1b6c8e37014050452402507a6656f47
Lucile <developer.lvt@gmail.com>
2021-07-01 11:26:10 -0400

    added useEffect hook to re-render the window title

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,6 +3,10 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props;
+  
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -24,7 +28,9 @@ function Nav(props) {
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+            <li className={`mx-1 ${
+              currentCategory.name === category.name && 'navActive'
+              }`} key={category.name}>
               <span onClick={() => setCurrentCategory(category)}>
                 {capitalizeFirstLetter(category.name)}
               </span>
