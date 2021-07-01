454b86706c9738d934de6e3cd407641b91c7b656
Lucile <developer.lvt@gmail.com>
2021-07-01 08:01:33 -0400

    added test to about component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
-
-  function categorySelected(name) {
-    console.log(`${name} clicked`)
-  }
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
 
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
         <a href="/">
           <span role="img" aria-label="camera">
@@ -40,7 +25,9 @@ function Nav() {
           </li>
           {categories.map((category) => (
             <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+              <span onClick={() => setCurrentCategory(category)}>
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
           ))}
         </ul>
